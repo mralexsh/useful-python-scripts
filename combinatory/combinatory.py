fdcd601f3cd163a9cfa8431dc4101a194f1b56ac
sas <sas@it-expertise.ru>
2024-03-19 16:44:18 +0700

    Add permutation logic

--- combinatory/combinatory.py
+++ combinatory/combinatory.py
@@ -3,6 +3,10 @@ import itertools
 
 passDict = ["1", "2", "3", "4", "5"]
 
+def generate_values(l):
+  L = list(map(lambda x: ''.join(x) ,list(itertools.permutations(l, len(l)))))
+  for e in L:
+    print(e)
 
 def make_subset(d, fn):
   for L in range(2, 5):
@@ -10,13 +14,13 @@ def make_subset(d, fn):
         fn(list(subset))
 
 def brute_force_dict(d):
-  make_subset(d, print)
+  make_subset(d, generate_values)
 
 def main(argv):
   if len(argv) == 0:
     print("Error: Missing input file")
   else:
-    brute_force_dict(passDict)
+    print(brute_force_dict(passDict))
     
 if (__name__ == '__main__'):
   main(sys.argv[1:])
